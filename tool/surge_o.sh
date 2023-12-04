b69fe38293b082ee5b103d9a2427b1d9bbb2fa31
QiuChenly <QiuChenly@outlook.com>
2023-12-04 09:19:10 +0800

    FIX 2532中导致Helper安装失败的问题

--- tool/surge_o.sh
+++ tool/surge_o.sh
@@ -48,10 +48,10 @@ src_info='/Applications/Surge.app/Contents/Info.plist'
 /usr/libexec/PlistBuddy -c "Set :SMPrivilegedExecutables:com.nssurge.surge-mac.helper \"identifier \\\"com.nssurge.surge-mac.helper\\\"\"" "$src_info"
 # /usr/libexec/PlistBuddy -c 'Print SMPrivilegedExecutables' "$src_info"
 
+sudo cp $PWD/tool/libInjectLib_Legecy.dylib /Applications/Surge.app/Contents/Frameworks/libInjectLib.dylib
+
 codesign -f -s - --all-architectures --deep /Applications/Surge.app/Contents/Library/LaunchServices/com.nssurge.surge-mac.helper
 codesign -f -s - --all-architectures --deep /Applications/Surge.app
 # python /Users/qiuchenly/Downloads/SMJobBless/SMJobBlessUtil.py check /Applications/Surge.app
-
-sudo cp $PWD/tool/libInjectLib_Legecy.dylib /Applications/Surge.app/Contents/Frameworks/libInjectLib.dylib
 
 echo "恭喜你！你的Mac已经被我植入了后门程序,现在即将结束整个进程，特工已经在对面楼中布下天罗地网，请主动自首争取宽大处理(虽然宽大不了几天)，记得下辈子不要用盗版软件🙏。\n"
